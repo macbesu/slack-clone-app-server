7658105e72202abef60a6f9a0509a1ce1793a806
ivanwarke <iveon.cc@gmail.com>
2018-04-14 00:38:19 +0800

    register

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,9 +1,19 @@
+import { hashSync, compareSync } from 'bcrypt-nodejs';
+
 export default {
   Query: {
     getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll(),
   },
   Mutation: {
-    createUser: (parent, args, { models }) => models.User.create(args),
+    register: async (parent, { password, ...otherArgs }, { models }) => {
+      try {
+        const hashedPassword = await hashSync(password);
+        await models.User.create({ ...otherArgs, hashedPassword });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
   },
 };
