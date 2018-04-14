f9538fd151dbf09f02c9fef1a26d81f1a4d7e00f
ivanwarke <iveon.cc@gmail.com>
2018-04-14 16:22:09 +0800

    register

--- resolvers/user.js
+++ resolvers/user.js
@@ -9,7 +9,7 @@ export default {
     register: async (parent, { password, ...otherArgs }, { models }) => {
       try {
         const hashedPassword = await hashSync(password);
-        await models.User.create({ ...otherArgs, hashedPassword });
+        await models.User.create({ ...otherArgs, password: hashedPassword });
         return true;
       } catch (err) {
         return false;
