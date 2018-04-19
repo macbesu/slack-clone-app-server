2735bad32c144bd4cb781a8b6240853ce9216810
ivanwarke <iveon.cc@gmail.com>
2018-04-19 18:17:55 +0800

    create a team

--- auth.js
+++ auth.js
@@ -28,6 +28,41 @@ export const createTokens = async (user, secret, secret2) => {
   
 };
 
+export const refreshTokens = async (token, refreshToken, models, SECRET, SECRET2) => {
+  let userId = -1;
+  try {
+    const { user: { id } } = await jwt.decode(refreshToken);
+    userId = id; 
+  } catch (err) {
+    return {};
+  }
+
+  if (!userId) {
+    return {};
+  }
+
+  const user = await models.User.findOne({ where: { id: userId }, raw: true });
+
+  if (!user) {
+    return {};
+  }
+
+  const refreshSecret = user.password + SECRET2;
+
+  try {
+    jwt.verify(refreshToken, refreshSecret);
+  } catch (err) {
+    return {};
+  }
+
+  const [newToken, newRefreshToken] = await createTokens(user, SECRET, refreshSecret);
+  return {
+    token: newToken,
+    refreshToken: newRefreshToken,
+    user,
+  };
+};
+
 export const tryLogin = async (email, password, models, SECRET, SECRET2) => {
   const user = await models.User.findOne({ where: { email }, raw: true });
   if (!user) {
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import { fileLoader, mergeTypes, mergeResolvers } from 'merge-graphql-schemas';
 import cors from 'cors';
 
 import models from './models';
+import { refreshTokens } from './auth';
 
 const SECRET = 'ivanchean';
 const SECRET2 = 'ivanchean2';
@@ -23,19 +24,39 @@ const schema = makeExecutableSchema({
 const app = express();
 app.use(cors('http://localhost:3001')); //app.use(cors('*'));
 
+const addUser = async (req, res, next) => {
+  const token = req.headers['x-token'];
+  if (token) {
+    try {
+      const { user } = jwt.verify(token, SECRET);
+      req.user = user;
+    } catch (err) {
+      const refreshToken = req.headers['x-refresh-token'];
+      const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+      if (newTokens.token && newTokens.token) {
+        res.set('Access-Control-Expose-Headers', 'x-token, x-refresh-token');
+        res.set('x-token', newTokens.token);
+        res.set('x-refresh-token', newTokens.refreshToken);
+      }
+      req.user = newTokens.user;
+    }
+  }
+  next();
+};
+
+app.use(addUser);
+
 const graphqlEndpoint = '/graphql';
 
-app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress({ 
+app.use(graphqlEndpoint, bodyParser.json(), graphqlExpress(req => ({ 
   schema,
   context: {
     models,
-    user: {
-      id: 1,
-    },
+    user: req.user,
     SECRET,
     SECRET2,
   },
-}));
+})));
 
 app.use('/graphiql', graphiqlExpress({ endpointURL: graphqlEndpoint }));
 
