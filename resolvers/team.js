e769d0dfefd3e18f26f643314db610305019008f
ivanwarke <iveon.cc@gmail.com>
2018-05-11 00:36:09 +0800

    [Add] Switching Teams with React Router Dynamic Routes

--- resolvers/team.js
+++ resolvers/team.js
@@ -10,9 +10,10 @@ export default {
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
         const team = await models.Team.create({ ...args, owner: user.id });
-        models.Channel.create({ name: 'general', public: true, teamId: team.id });
+        await models.Channel.create({ name: 'general', public: true, teamId: team.id });
         return {
           ok: true,
+          team,
         };
       } catch (err) {
         console.log(err);
