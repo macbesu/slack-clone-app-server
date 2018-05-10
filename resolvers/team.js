362d9d9966d6f9e59fea9278cce9756571dd08ff
ivanwarke <iveon.cc@gmail.com>
2018-05-11 00:12:33 +0800

    [Update] Team resolver and Channel resolver

--- resolvers/team.js
+++ resolvers/team.js
@@ -9,7 +9,8 @@ export default {
   Mutation: {
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        await models.Team.create({ ...args, owner: user.id });
+        const team = await models.Team.create({ ...args, owner: user.id });
+        models.Channel.create({ name: 'general', public: true, teamId: team.id });
         return {
           ok: true,
         };
@@ -23,6 +24,6 @@ export default {
     }),
   },
   Team: {
-    channels: ({ id }, args, { models }) => models.Channel.findAll({ teamId: id }),
+    channels: ({ id }, args, { models }) => models.Channel.findAll({ where: { teamId: id } }),
   }
 };
