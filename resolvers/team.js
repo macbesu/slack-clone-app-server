143fffc2f818acc0e77e5249407772c1e7785159
ivanwarke <iveon.cc@gmail.com>
2018-05-15 14:32:32 +0800

    [Update] Team Resolver

--- resolvers/team.js
+++ resolvers/team.js
@@ -10,15 +10,24 @@ export default {
     addTeamMember: requiresAuth.createResolver( async (parent, { email, teamId }, { models, user }) => {
       try {
         const teamPromise = models.findOne({ where: { id: teamId } }, { raw: true });
-        const userToAdd = models.findOne({ where: { email } }, { raw: true });
-
+        const userToAddPromise = models.findOne({ where: { email } }, { raw: true });
+        const [team, userToAdd] = await Promise.all([teamPromise, userToAddPromise]);
         if (team.owner !== user.id) {
           return {
             ok: false,
+            errors: [{ path: 'email', message: 'You cannot add members to the team!' }],
           };
         }
-        
+        if (!userToAdd) {
+          return {
+            ok: false,
+            errors: [{ path: 'email', message: 'Could not find user with this email!' }],
+          };
+        }
         await models.Member.create({ userId: userToAdd.id, teamId });
+        return {
+          ok: true,
+        };
       } catch (err) {
         console.log(err);
         return {
