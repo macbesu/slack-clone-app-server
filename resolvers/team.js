a7cd316c7b4d7dbe695b51a5c23bddeba89d5700
ivanwarke <iveon.cc@gmail.com>
2018-05-24 19:17:45 +0800

    [Add]Transactions

--- resolvers/team.js
+++ resolvers/team.js
@@ -38,17 +38,20 @@ export default {
     }),
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const team = await models.Team.create({ ...args, owner: user.id });
-        await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+        const res = await models.sequelize.transaction(async () => {
+          const team = await models.Team.create({ ...args, owner: user.id });
+          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
+          return team;
+        });
         return {
           ok: true,
-          team,
+          team: res,
         };
       } catch (err) {
         console.log(err);
         return {
           ok: false,
-          errors: formatErrors(err),
+          errors: formatErrors(err, models),
         };
       }
     }),
