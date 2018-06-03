3466816104a6221c0d226ffcd8d4aa582c56dbe2
ivanwarke <iveon.cc@gmail.com>
2018-06-03 13:07:54 +0800

    [Add] GraphQL Subscriptions Authentication

--- index.js
+++ index.js
@@ -84,6 +84,31 @@ models.sequelize.sync().then(() => {
         execute,
         subscribe,
         schema,
+        onConnect: async ({ token, refreshToken }, webSocket) => {
+          if (token && refreshToken) {
+            let user = null;
+            try {
+              const payload = jwt.verify(token, SECRET);
+              user = payload.user;
+            } catch (err) {
+              const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+              user = newTokens.user;
+            }
+            if (!user) {
+              throw new Error('Invaid auth tokens');
+            }
+
+            const member = await models.Member.findOne({ where: { teamId: 1, userId: user.id } });
+
+            if (!member) {
+              throw new Error('Missing auth tokens!');
+            }
+
+            return true;
+          }
+
+          throw new Error('Missing auth tokens!');
+        },
       },
       {
         server,
